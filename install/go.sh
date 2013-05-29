0ff33325b7ef36bec89413ca756fb8ba6410bea3
Michael Crosby <crosby.michael@gmail.com>
2013-05-28 20:43:05 -0400

    Update backup and go install
    Update go PATH and envs

--- install/go.sh
+++ install/go.sh
@@ -5,8 +5,11 @@ echo "Installing go toolchain to /usr/local"
 esys=$(uname)
 if [ $esys = Darwin ]
     then
-       wget -O - https://go.googlecode.com/files/go1.1.darwin-amd64.tar.gz | tar -zxvf -C /usr/local
+       wget -O go.tar.gz https://go.googlecode.com/files/go1.1.darwin-amd64.tar.gz 
     else
-        wget -O - https://go.googlecode.com/files/go1.1.linux-amd64.tar.gz | tar -zxvf -C /usr/local
+        wget -O go.tar.gz https://go.googlecode.com/files/go1.1.linux-amd64.tar.gz | tar -zxvf -C /usr/local
 fi
 
+tar -zxvf go.tar.gz -C /usr/local
+
+rm go.tar.gz
