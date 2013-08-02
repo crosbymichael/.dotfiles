a3f0a2d6242e5153622d3e1c29c4c376f8ba38d1
Michael Crosby <crosby.michael@gmail.com>
2013-08-01 21:33:16 -0400

    Changes to git

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,13 @@
 
 # Compiled apps: git tig watchman python go
 
-apt-get update
-apt-get upgrade -y
+# apt-get update
+# apt-get upgrade -y
 
-apt-get install -y htop wget curl tmux zsh gcc g++ build-essential make automake ctags vim irssi python-dev libc6-dev autoconf \
-bison cpp gawk gdb gettext sqlite3 libsqlite3-dev ncurses ncurses-dev mercurial
-
+apt-get install -y htop wget curl tmux zsh gcc g++ build-essential make automake ctags vim irssi python-dev libc6-dev autoconf bison cpp gawk gdb gettext sqlite3 libsqlite3-dev ncurses-dev mercurial
 
 # Python deps
+cd /home/vagrant
 
 wget http://python.org/ftp/python/2.7.5/Python-2.7.5.tgz
 
@@ -31,20 +30,22 @@ wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 
 pip install virtualenv
 
-cd ~/.dotfiles
+cd /home/vagrant
 
 # Compiles
 
 
 # Setup git
 
-wget -O git.tar.gz https://git-core.googlecode.com/files/git-1.8.3.4.tar.gz
-tar -zxvf git.tar.gz
-rm git.tar.gz
-cd git
+wget https://git-core.googlecode.com/files/git-1.8.3.4.tar.gz
+tar -zxvf git-1.8.3.4.tar.gz
+rm git-1.8.3.4.tar.gz
+
+cd git-1.8.3.4/
 ./configure --prefix=/usr/local
 make && make install
-cd ~/.dotfiles
+
+cd /home/vagrant
 
 # Go deps
 wget -O go.tar.gz https://go.googlecode.com/files/go1.1.1.linux-amd64.tar.gz | tar -zxvf -C /usr/local
@@ -52,6 +53,7 @@ tar -zxvf go.tar.gz -C /usr/local
 
 rm go.tar.gz
 
+cd /home/vagrant
 #go get github.com/golang/lint/golint
 #go get github.com/nsf/gocode
 #go get github.com/nsf/gocode
@@ -61,23 +63,21 @@ rm go.tar.gz
 apt-get install -y lxc aufs-tools
 
 # Install dotfiles
-cd ~/
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
-./install.sh
 
-rm install.sh
+cd /home/vagrant
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+cd /home/vagrant
 
 # Copy dotfiles into home dir
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
-ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s /home/vagrant/.dotfiles/vim /home/vagrant/.vim
+ln -s /home/vagrant/.dotfiles/vimrc /home/vagrant/.vimrc
+ln -s /home/vagrant/.dotfiles/git/gitconfig /home/vagrant/.gitconfig
+ln -s /home/vagrant/.dotfiles/git/gitignore_global /home/vagrant/.gitignore_global
+ln -s /home/vagrant/.dotfiles/zshrc /home/vagrant/.zshrc
+ln -s /home/vagrant/.dotfiles/tmux.conf /home/vagrant/.tmux.conf
 
 
 # Change default shell
 chsh -s /bin/zsh vagrant
-
 
 echo "Completed install..."
