db443720fe827f3f28db88d9a97757c7bff22005
Michael Crosby <crosby.michael@gmail.com>
2013-10-05 18:45:47 -0700

    Add vim compile script

---
+++ vim.sh
@@ -0,0 +1,10 @@
+./configure --prefix=/usr/local \\
+--enable-gui=no \\
+--without-x \\
+--disable-nls \\
+--enable-multibyte \\
+--with-tlib=ncurses \\
+--enable-pythoninterp \\
+--enable-rubyinterp \\
+--with-features=huge
+
