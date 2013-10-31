dccd575b100809eed243daa84b096a1ef4dcdeaf
Michael Crosby <crosby.michael@gmail.com>
2013-10-30 20:50:51 -0700

    Cannot echo sudoers update

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@
 
 # ssh michael@xxx "wget https://raw.github.com/crosbymichael/.dotfiles/master/init.sh && chmod +x init.sh && sudo ./init.sh michael"
 
-# Do not require sudo pass for user
-echo "$1 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
 apt-get update
 apt-get install -y git-core
 
