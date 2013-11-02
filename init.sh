a84b434f5723a12dcc64d64bf16137e3874f539e
Michael Crosby <crosby.michael@gmail.com>
2013-11-02 00:05:56 -0700

    Move apt install to init

--- init.sh
+++ init.sh
@@ -3,13 +3,48 @@
 # ssh michael@xxx "wget https://raw.github.com/crosbymichael/.dotfiles/master/init.sh && chmod +x init.sh && sudo ./init.sh michael"
 
 apt-get update
-apt-get install -y git-core
 
 export USER_NAME=$1
 
-git clone https://github.com/crosbymichael/.dotfiles
-cd .dotfiles/
-git submodule update --init
-./bootstrap.sh
+# infrastructure
+apt-get install -y \
+    gcc \
+    g++ \
+    build-essential \
+    make \
+    automake \
+    python \
+    python-dev \
+    libc6-dev \
+    autoconf \
+    bison \
+    cpp \
+    gawk \
+    gettext \
+    ncurses-dev \
+    mercurial \
+    lxc \
+    aufs-tools  \
+    #    supervisor 
+    libbz2-dev \
+    libreadline-dev \
+    ruby \
+    rubygems
 
-echo "All done"
+# applications
+apt-get install -y  \
+    wget \
+    iotop \
+    nmap \
+    curl \
+    socat \
+#   tmux \
+#   htop \
+#   irssi \
+#   gdb \
+#   tcpflow \
+#   ranger \
+#   lynx
+#   mutt
+    sysstat 
+
