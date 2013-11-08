40ebd5f8325569ee0b7632252946ae29d2a41fad
Michael Crosby <crosby.michael@gmail.com>
2013-11-07 22:31:44 -0800

    Add new simple prompt

--- config.fish
+++ config.fish
@@ -67,28 +67,34 @@ alias iptbleshow='iptables -L -n -t nat'
 alias lsa='ls -lah --color=auto'
 alias ls='ls -lh --color=auto'
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
 
-# Status Chars
-set __fish_git_prompt_char_dirtystate '☁'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
 
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
 
 function fish_prompt
-  set last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -l red (set_color -o red)
+  set -l blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -l normal (set_color normal)
 
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
+  set -l cwd $cyan(basename (prompt_pwd))
 
-  printf '%s ' (__fish_git_prompt)
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+    set git_info "$green$git_branch "
 
-  set_color normal
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow✗"
+      set git_info "$git_info$dirty"
+    end
+  end
+
+  echo -n -s $cwd $red '|' $git_info $normal ⇒ ' ' $normal
 end
