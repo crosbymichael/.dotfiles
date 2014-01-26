4ef34d8da09673bcd8f2ba002e09652248318f37
Michael Crosby <michael@crosbymichael.com>
2014-01-26 10:02:14 -0800

    Remove all files

--- go-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-go get -u github.com/golang/lint/golint
-go get -u github.com/nsf/gocode
-go get -u github.com/jstemmer/gotags
-go build -o bin/server bin/server.go
--- vim/ftdetect/arduino.vim
+++
@@ -1,1 +0,0 @@
-au! BufRead,BufNewFile *.pde setfiletype arduino
