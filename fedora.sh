04e6c7c1ff66bab68a5f7378bc9bf5eec8f52d46
Michael Crosby <michael@crosbymichael.com>
2014-02-05 10:48:45 -0800

    Switch back to bash from fish

--- fedora.sh
+++ fedora.sh
@@ -21,19 +21,14 @@ yum install \
 wget https://go.googlecode.com/files/go1.2.src.tar.gz && tar zxvf go1.2.src.tar.gz && \
 	cd go/src && ./all.bash && cd
 
-wget http://fishshell.com/files/2.1.0/fish-2.1.0.tar.gz && tar -zxf fish-2.1.0.tar.gz &&\
-	cd fish-2.1.0/ && ./configure --prefix=/usr/local && make && make install &&\
-	echo '/usr/local/bin/fish' | tee -a /etc/shells && chsh -s /usr/local/bin/fish &&\
-	cd && rm -rf fish-2.1.0 && rm fish-2.1.0.tar.gz
-
-
 ln -s /root/.dotfiles/vim /root/.vim && ln -s /root/.dotfiles/vimrc /root/.vimrc
-mkdir -p /root/.config/fish && ln -s /root/.dotfiles/config.fish /root/.config/fish/config.fish
 ln -s /root/.dotfiles/tmux.conf /root/.tmux.conf
 ln -s /root/.dotfiles/git/gitconfig /root/.gitconfig
 ln -s /root/.dotfiles/git/gitignore_global /root/.gitignore_global
 ln -s /root/.dotfiles/NERDTreeBookmarks /root/.NERDTreeBookmarks
 ln -s /root/.dotfiles/sshconf  /root/.ssh/config
+rm /root/.bashrc
+ln -s /root/.dotfiles/bashrc  /root/.bashrc
 
 systemctl disable firewalld
 systemctl stop firewalld
