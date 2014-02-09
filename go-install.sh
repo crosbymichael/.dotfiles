ee10421001e45c96523619919cb0b01e6433ac70
Michael Crosby <michael@crosbymichael.com>
2014-02-09 00:15:58 -0800

    Update goimports

--- go-install.sh
+++ go-install.sh
@@ -5,3 +5,4 @@ go get -u github.com/nsf/gocode
 go get -u github.com/jstemmer/gotags
 go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u github.com/rakyll/boom
+go get -u code.google.com/p/go.tools/cmd/goimports
