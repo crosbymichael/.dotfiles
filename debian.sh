0a712360af9aae7852314b0d63c829db8bb3ae65
Michael Crosby <michael@crosbymichael.com>
2014-02-23 12:34:06 -0800

    Fix docker symlink

--- debian.sh
+++ debian.sh
@@ -33,7 +33,6 @@ wget http://fishshell.com/files/2.1.0/fish-2.1.0.tar.gz && tar -zxf fish-2.1.0.t
 	echo '/usr/local/bin/fish' | tee -a /etc/shells && chsh -s /usr/local/bin/fish &&\
 	cd && rm -rf fish-2.1.0 && rm fish-2.1.0.tar.gz
 
-
 ln -s /root/.dotfiles/vim /root/.vim && ln -s /root/.dotfiles/vimrc /root/.vimrc
 mkdir -p /root/.config/fish && ln -s /root/.dotfiles/config.fish /root/.config/fish/config.fish
 ln -s /root/.dotfiles/tmux.conf /root/.tmux.conf
@@ -42,4 +41,3 @@ ln -s /root/.dotfiles/git/gitignore_global /root/.gitignore_global
 ln -s /root/.dotfiles/NERDTreeBookmarks /root/.NERDTreeBookmarks
 mkdir -p /root/.ssh
 ln -s /root/.dotfiles/sshconf  /root/.ssh/config
-ln -s /.dockerinit /usr/local/bin/docker
