250e5e69be363e2a07813c449b507b23df1b15d0
Michael Crosby <michael@crosbymichael.com>
2014-04-11 09:21:47 -0700

    Update goinstall to not update
    I could have a dirty working tree

--- go-install.sh
+++ go-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go get -u github.com/golang/lint/golint
-go get -u github.com/nsf/gocode
-go get -u github.com/jstemmer/gotags
-go get -u code.google.com/p/rog-go/exp/cmd/godef
-go get -u github.com/rakyll/boom
-go get -u code.google.com/p/go.tools/cmd/goimports
+go get github.com/golang/lint/golint
+go get github.com/nsf/gocode
+go get github.com/jstemmer/gotags
+go get code.google.com/p/rog-go/exp/cmd/godef
+go get github.com/rakyll/boom
+go get code.google.com/p/go.tools/cmd/goimports
