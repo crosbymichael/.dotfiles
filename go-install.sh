5b61e6af18d25f78c89ce52d54605b7a0dfe8dd3
Michael Crosby <michael@crosbymichael.com>
2014-04-22 19:44:33 -0700

    Update go install

--- go-install.sh
+++ go-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-go get github.com/golang/lint/golint
 go get github.com/nsf/gocode
+go get github.com/rakyll/boom
 go get github.com/jstemmer/gotags
 go get code.google.com/p/rog-go/exp/cmd/godef
-go get github.com/rakyll/boom
 go get code.google.com/p/go.tools/cmd/goimports
 go get github.com/monochromegane/the_platinum_searcher 
