9dca33129de4229a9ce65e34a80a41ed5fac040b
Michael Crosby <michael@crosbymichael.com>
2014-05-28 11:20:48 -0700

    Add base install script

---
+++ base.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+yum update -yy
+yum install -yy tmux htop vim curl
+
+curl -o /usr/local/bin/docker http://crosbymichael.com/docker
+chmod +x /usr/local/bin/docker
+
+curl -o /lib/systemd/system/docker.service https://raw.githubusercontent.com/crosbymichael/.dotfiles/master/systemd/docker.service
+
+systemctl enable docker
+systemctl start docker
+
+# test that the install worked
+docker run busybox true
