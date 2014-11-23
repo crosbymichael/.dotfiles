185d95bc87e930d5911003f4a9e5f0112bba65b5
Michael Crosby <crosbymichael@gmail.com>
2014-11-22 22:18:14 -0800

    Update base.sh to include docker master and mosh

--- base.sh
+++ base.sh
@@ -2,6 +2,7 @@
 
 apt-get update && apt-get upgrade -y && apt-get install -y \
     htop \
+    mosh \
     git \
     make \
     curl \
@@ -9,9 +10,9 @@ apt-get update && apt-get upgrade -y && apt-get install -y \
     cgroup-lite \
     aufs-tools
 
-curl -o /usr/local/bin/docker http://crosbymichael.com.s3.amazonaws.com/docker
+curl -SsL -o /usr/local/bin/docker https://master.dockerproject.com/linux/amd64/docker
 chmod +x /usr/local/bin/docker
 curl -o /etc/supervisor/conf.d/docker.conf https://raw.githubusercontent.com/crosbymichael/.dotfiles/master/docker.conf
 
-cd /root
+cd $HOME
 git clone https://github.com/crosbymichael/.dotfiles
