a379b78bbc345c0b558a8ad855fdc21cf5643174
Michael Crosby <crosbymichael@gmail.com>
2015-02-07 14:07:44 -0800

    Add docker-compose

--- core.sh
+++ core.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# install system dependencies 
 apt-get install -y \
     htop \
     git \
@@ -11,7 +12,11 @@ apt-get install -y \
     cgroup-lite \
     aufs-tools
 
+# install docker from master
 curl -SsL -o /usr/local/bin/docker https://master.dockerproject.com/linux/amd64/docker
 chmod +x /usr/local/bin/docker
 curl -o /etc/supervisor/conf.d/docker.conf https://raw.githubusercontent.com/crosbymichael/.dotfiles/master/docker.conf
+
+# install docker-compose
+curl -L https://github.com/docker/fig/releases/download/1.1.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose; chmod +x /usr/local/bin/docker-compose
 
