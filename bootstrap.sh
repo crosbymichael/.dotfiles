e47331225b4fc8550fcb56f298d897447bd2321e
Michael Crosby <crosbymichael@gmail.com>
2018-04-18 13:20:28 -0400

    Update bootstrap script for symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ function install_base () {
         libapparmor-dev \
         libseccomp-dev \
         make \
-        tmux 
+        tmux
 }
 
 # install additional go deps
@@ -28,7 +28,7 @@ function go_deps() {
     go get github.com/crosbymichael/gistit
     go get github.com/crosbymichael/slex
     go get github.com/jstemmer/gotags
-    go get github.com/monochromegane/the_platinum_searcher 
+    go get github.com/monochromegane/the_platinum_searcher
     go get github.com/nsf/gocode
 }
 
@@ -47,45 +47,44 @@ function install_dev() {
         libc6-dev \
         libclang-dev \
         libtool \
-        make \
         man-db \
         manpages-dev \
         mercurial \
         openssh-client \
         procps \
-        tig \
         tree \
         valgrind \
         pkg-config \
         vim-nox
-
 }
 
-
-function tools () {
-    # install go
-    (
+function go_install () {
+	(
         cd $HOME
         curl -s https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz | tar -zxf -
     )
+}
 
+function setup_home () {
     cd $HOME
 
     rm -rf $HOME/.vim
-    ln -s $HOME/.dotfiles/vim $HOME/.vim && ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc 
-    ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf 
-    rm -f $HOME/.gitconfig && ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig 
-    ln -s $HOME/.dotfiles/gitignore_global $HOME/.gitignore_global 
-    ln -s $HOME/.dotfiles/NERDTreeBookmarks $HOME/.NERDTreeBookmarks 
-    mkdir -p $HOME/.ssh && ln -s $HOME/.dotfiles/sshconf  $HOME/.ssh/config 
-    rm -f $HOME/.bashrc && ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc
+    ln -s $HOME/.dotfiles/vim $HOME/.vim || true
+	ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc || true
+    ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf || true
+    rm -f $HOME/.gitconfig && ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig || true
+    ln -s $HOME/.dotfiles/gitignore_global $HOME/.gitignore_global || true
+    ln -s $HOME/.dotfiles/NERDTreeBookmarks $HOME/.NERDTreeBookmarks || true
+    mkdir -p $HOME/.ssh && ln -s $HOME/.dotfiles/sshconf  $HOME/.ssh/config || true
+    rm -f $HOME/.bashrc && ln -s $HOME/.dotfiles/bashrc $HOME/.bashrc || true
 
-    ln -s $HOME/development/gocode/src/github.com/docker/docker $HOME/docker
-    ln -s $HOME/development/gocode/src/github.com/containerd $HOME/containerd
-    ln -s $HOME/development/gocode/src/github.com/opencontainers/runc $HOME/runc
+    ln -s $HOME/development/gocode/src/github.com/docker/docker $HOME/docker || true
+    ln -s $HOME/development/gocode/src/github.com/containerd $HOME/containerd  || true
+    ln -s $HOME/development/gocode/src/github.com/opencontainers/runc $HOME/runc || true
+    ln -s $HOME/development/gocode/src/github.com $HOME/github || true
 
     # additional home directories
-    mkdir $HOME/scratch
+    mkdir -p $HOME/scratch
 }
 
 # main entrypoint
@@ -93,17 +92,22 @@ case "$1" in
     "server")
         echo "installing server dependencies..."
         update
-        install_base 
+        install_base
         ;;
     "dev")
         echo "installing development dependencies..."
         update
         install_base
         install_dev
-		tools
+		go_install
+		setup_home
         source $HOME/.bashrc
-        go_deps 
+        go_deps
         ;;
+	"home")
+		echo "Setup home dir..."
+		setup_home
+		;;
     *)
         echo "invalid install option"
         echo "the two options are for a 'server' install and a 'dev' install"
